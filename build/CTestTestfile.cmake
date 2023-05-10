daf97f1edc96941740a2f1e9306e1e11092dac8c
Denys Madureira <denysmb@zoho.com>
2023-05-10 13:01:07 +0100

    feat: remove build folder and add .gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/denys/Projects/ChatQTP
-# Build directory: /home/denys/Projects/ChatQTP/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(appstreamtest "/usr/bin/cmake" "-DAPPSTREAMCLI=/usr/bin/appstreamcli" "-DINSTALL_FILES=/home/denys/Projects/ChatQTP/build/install_manifest.txt" "-P" "/usr/share/ECM/kde-modules/appstreamtest.cmake")
-set_tests_properties(appstreamtest PROPERTIES  _BACKTRACE_TRIPLES "/usr/share/ECM/kde-modules/KDECMakeSettings.cmake;165;add_test;/usr/share/ECM/kde-modules/KDECMakeSettings.cmake;183;appstreamtest;/usr/share/ECM/kde-modules/KDECMakeSettings.cmake;0;;/home/denys/Projects/ChatQTP/CMakeLists.txt;18;include;/home/denys/Projects/ChatQTP/CMakeLists.txt;0;")
-subdirs("src")
--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/denys/Projects/ChatQTP/src
-# Build directory: /home/denys/Projects/ChatQTP/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
